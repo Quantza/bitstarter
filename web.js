9e637a1614504151c7011d2d06b4c3618116a716
Luther P <post2base@outlook.com>
2013-09-07 13:43:25 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var html = fs.readFile(index.html);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(html.toString());
 });
 
 var port = process.env.PORT || 5000;
